e34eab33894f2a5acfd084bce544e5f8901f2cb2
Alexander <138435791+Skullester@users.noreply.github.com>
2023-07-07 13:23:20 +0400

    Wrote script

--- Haunted Dices/Assets/Scripts/Dice.cs
+++ Haunted Dices/Assets/Scripts/Dice.cs
@@ -1,24 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Dice : MonoBehaviour
 {
-    private bool isCollision;
-
     // Start is called before the first frame update
     void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!isCollision)
-            RotateCube();
-    }
+    void Update() { }
 
     public void RollDice()
     {
-        isCollision = false;
         int randomNumber = GetRandomNumber();
         Debug.Log(randomNumber);
     }
@@ -28,15 +22,5 @@ public class Dice : MonoBehaviour
         int minScore = 1,
             maxScore = 12;
         return Random.Range(minScore, maxScore + 1);
-    }
-
-    void RotateCube()
-    {
-        transform.Rotate(new Vector3(0f, -20f, -80f) * Time.deltaTime);
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        isCollision = true;
     }
 }
