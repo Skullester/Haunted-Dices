0fe74ff289f1be69d09e5fae0d29383a8a9c67d1
Alexander <138435791+Skullester@users.noreply.github.com>
2023-07-16 20:50:29 +0400

    added tutorial

---
+++ Haunted Dices/Assets/Scripts/Scene 1/Education.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class Education : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterMoving cm;
+    private int count = 1;
+
+    [SerializeField]
+    private GameObject btn;
+
+    [SerializeField]
+    private GameObject[] objects;
+    public static bool isEducationPassed; //костыль
+
+    public void GotItButton()
+    {
+        if (count == objects.Length)
+        {
+            isEducationPassed = true;
+            cm.enabled = true;
+            gameObject.SetActive(false);
+            return;
+        }
+        objects[count - 1].SetActive(false);
+        objects[count++].SetActive(true);
+    }
+
+    private void Start()
+    {
+        if (isEducationPassed)
+            gameObject.SetActive(false);
+        cm.enabled = false;
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(0.3f);
+        objects[0].SetActive(true);
+        btn.SetActive(true);
+    }
+}
